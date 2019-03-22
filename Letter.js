a7e10cccdf8b3a213b9258b71b20ecf535e2b8aa
cfcrotty <carafeliselana@gmail.com>
2019-03-22 15:03:20 -0700

    update

--- Letter.js
+++ Letter.js
@@ -1,19 +1,20 @@
 const Letter = function (letter) {
     this.letter = letter;
     this.isGuessed = false;
+    if (this.letter === " ") {
+        this.isGuessed = true;
+    }
     this.getLetter = function () {
         if (this.isGuessed) return this.letter;
         else return "_";
     };
     this.updateLetter = function (varLetter) {
-        if (this.letter === " ") {
+
+        if (this.letter.toLowerCase() === varLetter.toLowerCase()) {
             this.isGuessed = true;
-        } else {
-            if (this.letter.toLowerCase() === varLetter.toLowerCase()) {
-                this.isGuessed = true;
-                return true;
-            }
+            return true;
         }
+
         return false;
     }
 
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const colors = require('colors');
 var fs = require("fs");
 const word = require("./Word.js");
 
-const words = ["javascript", "server side"];
+const words = ["javascript", "server side","template literal","java","mysql"];
 var remainingGuess = 10;
 var guessWord = "";
 var canPlay = true;
@@ -11,16 +11,11 @@ var numWord = 0;
 var lettersGuessed = [];
 var wordsDone = [];
 
-/*
-const guessWord = new word("the");
-console.log(guessWord.wordString());
-console.log(guessWord.checkCharacter("t"));
-console.log(guessWord.checkCharacter("h"));
-console.log(guessWord.wordString());
-*/
 console.log("Word Guess Game Starting...".cyan);
 console.log("Type exit then press Enter to end the game.".red);
-//askQuestion();
+/**
+ * Function to ask question
+ */
 function askQuestion() {
     inquirer.prompt([
         {
@@ -52,17 +47,21 @@ function askQuestion() {
             canPlay = true;
             remainingGuess = 10;
             lettersGuessed = [];
-            playGame();
+            playNextWord();
         }
     });
 }
-
+/**
+ * Function to get random word from words array
+ */
 function getRandomWord() {
     var idx = Math.floor(Math.random() * words.length);
     var theWord = words[idx].trim().replace(/[^a-zA-Z ]/g, "");
     return theWord;
 }
-
+/**
+ * Function to play the game
+ */
 function playGame() {
     if (canPlay) {
         guessWord = getRandomWord();
@@ -78,16 +77,33 @@ function playGame() {
         remainingGuess = 10;
         canPlay = true;
         lettersGuessed = [];
-        playGame();
+        playNextWord();
     }
 }
-
+/**
+ * Function to check if character is in word
+ * @param {string} char -the letter to check
+ */
 function checkCharacter(char) {
     var isTrue = guessWord.checkCharacter(char);
     if (!isTrue) {remainingGuess--;
     console.log(`You have ${remainingGuess} remaining guess.`);
     }
-    //console.log(guessWord);
+}
+
+function playNextWord(){
+    inquirer.prompt([
+        {
+            type: "confirm",
+            message: "Do you want to continue?".red.italic.bold,
+            name: "playAgain",
+        }
+    ]).then(function (inquirerResponse) {
+        if (inquirerResponse.playAgain) {
+            playGame()
+        } else {
+            process.exit();
+        }
+    });
 }
 playGame();
-//track users remaining guess
